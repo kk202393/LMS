131fd633eccae85e2beb89501855d8d7b2ee08c2
Adglobal360-ui <142399582+Adglobal360-ui@users.noreply.github.com>
2024-05-06 22:21:54 +0530

    update server file

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,4 +3,5 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install --upgrade setuptools wheel 
+sudo apt-get install mysql-server
 pip install -r /home/ubuntu/LMS/requirements.txt
