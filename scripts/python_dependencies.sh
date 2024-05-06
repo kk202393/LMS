e82ed294629e662bc402bda037e5d401ba88c29b
Adglobal360-ui <142399582+Adglobal360-ui@users.noreply.github.com>
2024-05-06 22:38:22 +0530

    update server file

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,6 +3,6 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install --upgrade setuptools wheel 
-pip install --upgrade setuptools
+pip install --upgrade setuptools --user python
 sudo apt-get install libmysqlclient-dev
 pip install -r /home/ubuntu/LMS/requirements.txt
