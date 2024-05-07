0b0a23f5824f57884313ead9a52bc4c539d24610
Adglobal360-ui <142399582+Adglobal360-ui@users.noreply.github.com>
2024-05-07 22:12:32 +0530

    update

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/LMS/LMS/settings.py
+sed -i 's/\[]/\["52.66.99.153"]/' /home/ubuntu/LMS/LMS/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
